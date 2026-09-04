8daef770e9458e5369c770f8e4a5b50bb4501086
c15c <35086801+c15c@users.noreply.github.com>
2026-09-04 19:07:21 +1000

    chore: remove duplicated config from handover copy

--- scout-handover/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-export default {
-  images: {
-    // Source images are hot-linked from the page they were published on.
-    remotePatterns: [{ protocol: "https", hostname: "**" }]
-  }
-};
